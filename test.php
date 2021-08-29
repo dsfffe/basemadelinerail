d6ea433b92a454a121ddbc2368107ea2f3d6e033
MohammadSylix <87855290+MohammadSylix@users.noreply.github.com>
2021-08-29 21:24:08 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,132 +1,124 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors','1');
-ini_set('memory_limit' , '-1');
-ini_set('max_execution_time','0');
-ini_set('display_startup_errors','1');
+/**
+ * @var Creator : T.me//MrSylix
+ * @version SylixTeam PHP Member Baner By MrSylix v1.0.0
+ */
+
 
 if (!file_exists('madeline.php')) {
     copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
+
 include 'madeline.php';
 
 use \danog\MadelineProto\API;
 use \danog\Loop\Generic\GenericLoop;
 use \danog\MadelineProto\EventHandler;
 
+
 class XHandler extends EventHandler
 {
-    const Admins = [1919698445];
-    const Report = 'sir_aboli';
-    
+    const Admins = [0 => 1476130628];
+    const Report = '@MrMahdiii';
+
     public function getReportPeers()
     {
         return [self::Report];
     }
-    
     public function genLoop()
     {
         yield $this->account->updateStatus([
             'offline' => false
         ]);
-        /*yield $this->messages->sendMessage([
-            'peer'    => self::Admins[0],
-            'message' => 'Generic Loop Start At : ' . date('H:i:s')
-        ]);*/
         return 60000;
     }
-    
+
     public function onStart()
     {
         $genLoop = new GenericLoop([$this, 'genLoop'], 'update Status');
         $genLoop->start();
     }
-    
+
     public function onUpdateNewChannelMessage($update)
     {
         yield $this->onUpdateNewMessage($update);
     }
-    
+    public function getMessages($update, $id = [])
+    {
+        try {
+            $info = yield $this->getInfo($update);
+
+            $method = in_array($info['type'], ['bot', 'user', 'chat']) ? 'messages' : 'channels';
+            $params = ['id' => $id];
+
+            if (in_array($info['type'], ['channel', 'supergroup'])) {
+                $params['channel'] = $update;
+            }
+
+            $get = yield $this->$method->getMessages($params);
+            return $get;
+        } catch (Throwable $e) {
+            //throw new Exception($e->getMessage());
+        }
+    }
+
     public function onUpdateNewMessage($update)
     {
         if (time() - $update['message']['date'] > 2) {
             return;
         }
         try {
-            $msgOrig   = $update['message']['message']?? null;
-            $messageId = $update['message']['id']?? 0;
-            $fromId    = $update['message']['from_id']['user_id']?? 0;
-            $replyToId = $update['message']['reply_to']['reply_to_msg_id']?? 0;
-            $peer      = yield $this->getID($update);
-                
+            $msg             = isset($update['message']) ? $update['message'] : null;
+            $txt             = isset($update['message']['message']) ? $update['message']['message'] : null;
+            $msg_id          = isset($update['message']['id']) ? $update['message']['id'] : null;
+            $reply_to_msg_id = isset($update['message']['reply_to']['reply_to_msg_id']) ? $update['message']['reply_to']['reply_to_msg_id'] : null;
+            $user_id         = isset($update['message']['from_id']['user_id']) ? $update['message']['from_id']['user_id'] : null;
+            $com             = isset($update['message']['fwd_from']['saved_from_peer']) ? true : false;
+            $me              = yield $this->getSelf();
+            $peer            = yield $this->getInfo($update);
+            $chID            = yield $this->getID($update);
+            $chat_id         = $peer['bot_api_id'];
+            $type            = $peer['type'];
+
+
             if (isset($update['message']['fwd_from']['saved_from_peer'])){
-                yield $this->messages->sendMessage(['peer' => $peer, 'message' => ".", 'parse_mode' => 'Markdown', 'reply_to_msg_id' => $messageId]);
+                yield $this->messages->sendMessage(['peer' => $chID, 'message' => "kosam", 'parse_mode' => 'Markdown', 'reply_to_msg_id' => $msg_id]);
                 }
+                
+            
+            #ADMIN Commands
+            if (in_array($user_id, self::Admins) || $user_id == $me_id) {
 
-            if((in_array($fromId, self::Admins))) {
-                if(preg_match('/^[\/\#\!\.]?(ping|ربات)$/si', $msgOrig)) {
-                    yield $this->messages->sendMessage([
-                        'peer'            => $peer,
-                        'message'         => 'Pong !',
-                        'reply_to_msg_id' => $messageId
-                    ]);
-                }
-                elseif (preg_match('/^[\/\#\!]?(restart|ریستارت)$/si',$msgOrig)){
-                    yield $this->messages->sendMessage([
-                        'peer'            => $peer,
-                        'message'         => 'Restarted ...',
-                        'reply_to_msg_id' => $messageId
-                    ]);
-                    $this->restart();
-                }
+            
+                    
 
-                elseif(preg_match("/^[\/\#\!\.]?(ping)$/i", $msgOrig)){
-                    $load = sys_getloadavg();
-                    $bot = array('UpTime' => uptime(time() - $this->timen));
-                    yield $this->messages->editMessage(['peer' => $peer, 'id' => $messageId, 'message' => "𖣐 Ping Server : ( $load[0] Ms )\n𖣐 UpTime: ( {$bot['UpTime']} )", 'parse_mode' => 'MarkDown']);}
+       
 
-                elseif(preg_match('/^[\/\#\!\.]?(status|وضعیت|وضع|مصرف|usage)$/si', $msgOrig)){
-                    $answer = 'Memory Usage : ' . round(memory_get_peak_usage(true) / 1021 / 1024, 2) . ' MB';
-                    yield $this->messages->sendMessage([
-                        'peer'            => $peer,
-                        'message'         => $answer,
-                        'reply_to_msg_id' => $messageId
-                    ]);
-                }
+
             }
-        } catch (\Throwable $e){
-            $this->report("Surfaced: $e");
+        } catch (\Throwable $e) {
+            //$this->report("Surfaced: $e");
         }
     }
 }
-$settings = [
-    'app_info' => [
-        'api_id' => 3063880,
-        'api_hash' => 'e0f71a9affe6526b56ab254c0bea9b1b'],
 
+$settings = [
     'serialization' => [
         'cleanup_before_serialization' => true,
     ],
     'logger' => [
-        'max_size' => 1*1024*1024,
+        'max_size' => 1 * 1024 * 1024,
     ],
     'peer' => [
         'full_fetch' => false,
         'cache_all_peers_on_startup' => false,
     ],
-    'db'            => [
-        'type'  => 'mysql',
-        'mysql' => [
-            'host'     => 'localhost',
-            'port'     => '3306',
-            'user'     => 'User',
-            'password' => 'Pass',
-            'database' => 'Database',
-        ]
+    'app_info' => [
+        'api_id' => 1374236,
+        'api_hash' => '4df50166bf37f939cd10c71ece878b06'
     ]
 ];
 
 $bot = new \danog\MadelineProto\API('X.session', $settings);
 $bot->startAndLoop(XHandler::class);
-?>
